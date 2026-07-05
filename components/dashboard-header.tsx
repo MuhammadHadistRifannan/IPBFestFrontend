ead11899d70abe986b7560ed70b906e26b680a22
pratama1246 <theperfectpratama.stu@pnc.ac.id>
2026-07-06 05:30:57 +0700

    fix(dashboard-header): remove eslint-disable comment for setMounted effect

--- components/dashboard-header.tsx
+++ components/dashboard-header.tsx
@@ -25,6 +25,7 @@ export function DashboardHeader() {
   const [mounted, setMounted] = React.useState(false);
 
   React.useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
